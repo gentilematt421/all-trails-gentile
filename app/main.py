9223989314beab3a84dd772aff58fbb2115eaa58
Matthew Gentile <gentilematt421@gmail.com>
2025-06-27 20:03:49 -0400

    change title

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import streamlit as st
 
 # Page configuration
-st.set_page_config(page_title="AllTrails Career App", layout="centered")
+st.set_page_config(page_title="AllTrails Senior Product Analyst App", layout="centered")
 
 # Main content
 st.title("I want to work at AllTrails! 🥾🌲")
 st.markdown("This is a personal project to showcase my data skills, product thinking, and love for the outdoors.")
+
+
